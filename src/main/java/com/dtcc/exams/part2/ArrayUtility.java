9451fdfae4b0a9181d8c678238abf90114431aa2
Emad Ahmed <emad@jetpe.com>
2020-09-19 14:15:18 -0400

    Started, not yet finished

--- src/main/java/com/dtcc/exams/part2/ArrayUtility.java
+++ src/main/java/com/dtcc/exams/part2/ArrayUtility.java
@@ -1,9 +1,16 @@
 package com.dtcc.exams.part2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayUtility {
 
     public Integer[] merge(Integer[] array1, Integer[] array2) {
-        return null;
+    List list = new ArrayList(Arrays.asList(array1));
+    list.addAll(Arrays.asList(array2));
+    Integer [] array3 = list.toArray();
+        return array3;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
