b2e8b0212976fc18993e12ff18672f9310d3ac6f
Emad Ahmed <emad@jetpe.com>
2020-09-19 14:11:00 -0400

    Finished:
    git commit -m Finished:

--- src/main/java/com/dtcc/exams/part1/IntegerArrayUtilities.java
+++ src/main/java/com/dtcc/exams/part1/IntegerArrayUtilities.java
@@ -1,21 +1,34 @@
 package com.dtcc.exams.part1;
 
+
+import java.util.ArrayList;
+
 public class IntegerArrayUtilities {
 
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        if(array.length %2 == 0){
+            return true;
+        }
+        return false;
     }
 
     public Integer[] range(int start, int stop) {
-        return null;
+
+        int arr = (start-stop);
+        Integer[] range = new Integer[Math.abs(arr)+1];
+        for(int i = start; i <= stop; i++){
+            range[i]= i;
+        }
+        return range;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+
+        return (array[0] + array[1]);
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        return (array[0] * array[1]);
     }
 
 }
