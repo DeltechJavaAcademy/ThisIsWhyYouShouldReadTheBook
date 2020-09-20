56c18ef715aefcc8b07af23ae3eb96cde0514f3a
Apoorvashukla88 <apoorvashukla88@gmail.com>
2020-09-20 19:23:39 -0400

    part2 completed

--- src/main/java/com/dtcc/exams/part2/Router.java
+++ src/main/java/com/dtcc/exams/part2/Router.java
@@ -1,22 +1,46 @@
 package com.dtcc.exams.part2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Router {
+    Map<String,String> hash_map = new HashMap<String,String>();
 
     public void add(String path, String controller) {
+
+        hash_map.put(path, controller);
+
     }
 
     public Integer size() {
-        return null;
+        return this.hash_map.size();
     }
 
     public String getController(String path) {
-        return null;
+        String value=null;
+        for(Map.Entry key:hash_map.entrySet()){        //this is how you traverse through the map.(Set)
+            if(key.getKey().equals(path)){
+                value= (String) key.getValue();
+            }
+        }
+        return value;
     }
 
+
     public void update(String path, String studentController) {
+        for(Map.Entry key:this.hash_map.entrySet()) {
+            if (key.getKey().equals(path)) {
+                key.setValue(studentController);
+            }
+        }
     }
 
     public void remove(String path) {
+        for(Map.Entry key:this.hash_map.entrySet()) {
+            if (key.getKey().equals(path)) {
+                hash_map.remove(path);
+            }
+        }
     }
 
 }
