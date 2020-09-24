03b1814a2421e65cc24ebcdbefce16ed38bb9746
Emad Ahmed <emad@jetpe.com>
2020-09-24 18:27:41 -0400

    finished

--- src/main/java/com/dtcc/exams/part1/BasicUtilities.java
+++ src/main/java/com/dtcc/exams/part1/BasicUtilities.java
@@ -3,19 +3,31 @@ package com.dtcc.exams.part1;
 public class BasicUtilities {
 
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        if (value >= 5) {
+            return true;
+        } else
+            return false;
     }
 
+
     public Boolean isLessThan7(Integer value) {
-        return null;
+        if (value < 7) {
+            return true;
+        }
+        return false;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        if (valueToEvaluate >= 5 || valueToEvaluate <= 7) {
+            return true;
+        } else return false;
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+        String firstChar= String.valueOf(string.charAt(0));
+        if (character.toString().equalsIgnoreCase(firstChar)) {
+            return true;
+        } else
+            return false;
     }
-
 }
